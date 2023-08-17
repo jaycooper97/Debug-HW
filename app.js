d8460a081d1cc24b0a0e41e96065d1841f1c8223
jay cooper <15jacooper@gmail.com>
2023-08-16 21:26:06 -0400

    push

---
+++ app.js
@@ -0,0 +1,45 @@
+var j = 15;
+var k = 30;
+function add(j, k){
+    return j + k;
+} 
+const add = (j, k) => {
+    return j + k;
+}
+console.log(add(j, k));
+
+
+
+var n = 44;
+var l = 22;
+function divide(n, l){
+    return n / l;
+}
+const divide = (n, l) => {
+    return n / l;
+}
+console.log(divide(n, l));
+
+
+
+var o = 99;
+var p = 85;
+function multiply(o, p){
+    return o * p;
+}
+const multiply = (o, p) => {
+    return o * p;
+}
+console.log(multiply(o. p));
+
+
+
+var r = 63;
+var q = 11;
+function subtract(r, q){
+    return r - q;
+}
+const subtract = (r, q) => {
+    return r - q;
+}
+console.log(multiply(r, q));
